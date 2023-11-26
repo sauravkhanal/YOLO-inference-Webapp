51bd0839c1da86d167faf622273b2da9cde898f5
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-11-26 20:30:49 +0545

    saves input and inferred images with timestamp as name

--- backend/infer_yolo.py
+++ backend/infer_yolo.py
@@ -1,8 +1,33 @@
 from ultralytics import YOLO
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+import cv2
+import os
+
+
+def check_make_dir(*directories: str) -> None:
+    """
+    Checks if directory exists, creates if not
+
+    Args:
+        *directories: variable number of path of directory to check
+    """
+    [os.makedirs(directory) for directory in directories if not os.path.exists(directory)]
+    # both do same
+    # for directory in directories:
+    #     if not os.path.exists(directory):
+    #         os.makedirs(directory)
+
 
 # path declarations
+image_save_path = "./saved_images"
+
+input_save_path = image_save_path + "/input"
+output_save_path = image_save_path + "/output"
+
+check_make_dir(input_save_path, output_save_path)
+
 model_path = "./model/best.pt"
 yolo_model = YOLO(model_path, task='detect')
 
@@ -35,11 +60,13 @@ class ReturnDict(BaseModel):
 
 
 def infer(image):
+    timestamp = save_input(image)
     results = yolo_model.predict(source=image)
-    return convert_to_json(results)
+    output_image_url = save_output(results, timestamp)
+    return convert_to_json(results, output_image_url)
 
 
-def convert_to_json(results):
+def convert_to_json(results, output_image_url: str):
     result = results[0]
     names = result.names
 
@@ -48,7 +75,7 @@ def convert_to_json(results):
 
     # all detection of one image
     detection_result = DetectionResult(
-        imageUrl=generate_image_url(result),
+        imageUrl=output_image_url,
         numberOfDetection=n,
         detections=[]
     )
@@ -82,8 +109,22 @@ def convert_to_json(results):
     )
 
 
-def generate_image_url(result) -> str:
-    # plot image
-    # deploy on server
-    # retrieve url
-    return "temp url"
+def generate_url(filename) -> str:
+    return filename
+
+
+def save_input(image) -> str:
+    # timestamp for current date and time e.g. '2023-11-26_16-33-28'
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-3]
+    cv2.imwrite(f"{input_save_path}/{timestamp}.jpg", image)
+    return timestamp
+
+
+def save_output(results, timestamp: str) -> str:
+    # result is list of detections for single prediction
+    # might need modification in case of batch image / mp4 prediction.
+
+    image_array = results[0].plot()
+    cv2.imwrite(f"{output_save_path}/{timestamp}.jpg", image_array)
+    # print(f"{output_save_path}/{name}.jpg")
+    return generate_url(timestamp)

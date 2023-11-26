66a89e5707e4678473e0aca96128ecea27e8d8a0
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-11-26 13:56:46 +0545

    updates return json format

--- backend/infer_yolo.py
+++ backend/infer_yolo.py
@@ -7,26 +7,83 @@ model_path = "./model/best.pt"
 yolo_model = YOLO(model_path, task='detect')
 
 
+class Box(BaseModel):
+    x: float
+    y: float
+    width: float
+    height: float
+
+
+class Detection(BaseModel):
+    classNumber: int
+    name: str
+    confidence: float
+    box: Box
+
+
 # custom datatype for returning result of detection
-class DetectionsResult(BaseModel):
-    num_detections: int = 0
-    detections: list[dict] = []
+class DetectionResult(BaseModel):
+    imageUrl: str
+    numberOfDetection: int
+    detections: list[Detection]
+
+
+class ReturnDict(BaseModel):
+    statusCode: int
+    message: str
+    data: DetectionResult
 
 
 def infer(image):
     results = yolo_model.predict(source=image)
-    return convert_to_dict(results)
+    return convert_to_json(results)
 
 
-def convert_to_dict(results) -> str:
-    result_dict = DetectionsResult()
-    for result in results:  # iterate through every detection but for now only one so redundant
-        result_dict.num_detections = len(result.boxes)  # no of detected box
-        # num_detect = len(result.boxes)
-        for i in range(result_dict.num_detections):  # iterate through every box
-            result_dict.detections.append({
-                'cls': int(result.boxes[i].cls),
-                'conf': round(float(result.boxes[i].conf)*100, 2),
-                'box': [float(x) for x in list(result.boxes[i].xywh[0])]
-            })
-    return jsonable_encoder(result_dict)
+def convert_to_json(results):
+    result = results[0]
+    names = result.names
+
+    # num of box detected ie numberOfDetections
+    n = len(result.boxes)
+
+    # all detection of one image
+    detection_result = DetectionResult(
+        imageUrl=generate_image_url(result),
+        numberOfDetection=n,
+        detections=[]
+    )
+
+    # now iterate through every box to add result(class and coordinates) of detection (of one image)
+    for i in range(n):
+        data = result.boxes[i]
+
+        box_value = [float(x) for x in list(result.boxes[i].xywh[0])]
+
+        detection_instance = Detection(
+            classNumber=int(data.cls),
+            name=names[int(data.cls)],
+            confidence=round(float(data.conf) * 100, 2),
+            box=Box(
+                x=box_value[0],
+                y=box_value[1],
+                width=box_value[2],
+                height=box_value[3],
+            )
+        )
+
+        detection_result.detections.append(detection_instance)
+
+    return jsonable_encoder(
+        ReturnDict(
+            statusCode=200,
+            message="Inference Successful",
+            data=detection_result
+        )
+    )
+
+
+def generate_image_url(result) -> str:
+    # plot image
+    # deploy on server
+    # retrieve url
+    return "temp url"

97d3cbca8b288a6db865acc5c2decce85af310a3
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-12-11 22:51:48 +0545

    implements upload image fully

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import image1 from './images/a.jpg'
 import image2 from './images/b.jpg'
 
 
+
 function App() {
   const [userImg, setUserImg] = useState(image1);
   const [inferredImg, setInferredImg] = useState(image2)
@@ -22,12 +23,10 @@ function App() {
         <ImageCard imageUrl={userImg} imageName='uploadedImage' ></ImageCard>
         <ImageCard imageUrl={inferredImg} imageName='inferredImage' buttonName={"Download"} ></ImageCard>
 
-        <input id='selectImage' type='file' accept='image/*'hidden />
+        <input id='selectImage' type='file' accept='image/*' capture="environment" hidden onChange={(event)=>setUserImg(URL.createObjectURL(event.target.files[0]))}/>
         <Button className='btn' variant='contained'><label htmlFor="selectImage">Select Image</label></Button>
 
         <Button className='btn' variant='contained' onClick={()=>handleDownload(inferredImg)}>Download Image</Button>
-        {/* <Btn buttonName={"Upload image"}></Btn>
-        <Btn buttonName={"Download image"} on></Btn> */}
       </div>
 
       <Button className='btn' variant='contained'>Infer</Button>

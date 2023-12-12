c6834d88bbb5a136bb3b94a4e6d2fd1a79efa4fb
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-12-12 17:39:15 +0545

    moves inferred image to bottom on <768px

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -2,7 +2,7 @@ import './HeroSection.css';
 import { useState } from 'react';
 import ImageCard from './components/ImageCard.js';
 
-import {dragOverHandler, dropHandler } from './utilities/appUtilities.js'
+import { dragOverHandler, dropHandler } from './utilities/appUtilities.js'
 import handleDownload from './utilities/handleDownload.js'
 import handleInfer from './utilities/handleInfer.js';
 
@@ -19,18 +19,18 @@ function HeroSection() {
   const [rawImg, setRawImg] = useState();
 
   return (
-    <div onDrop={(event)=>dropHandler(event,setUserImg,setRawImg)} onDragOver={dragOverHandler}>
+    <div className='imShow' onDrop={(event) => dropHandler(event, setUserImg, setRawImg)} onDragOver={dragOverHandler}>
 
-      <div className='imShow'>
-        <ImageCard imageUrl={userImg} imageName='uploadedImage' imgText='original'></ImageCard>
-        <ImageCard imageUrl={inferredImg} imageName='inferredImage' buttonName={"Download"} imgText='inferred'></ImageCard>
-
-        <input id='selectImage' type='file' accept='image/*'  hidden onChange={(event)=>{setUserImg(URL.createObjectURL(event.target.files[0]));setRawImg(event.target.files[0])}}/>
-        <Button className='btn' variant='contained'><label htmlFor="selectImage">Select Image</label></Button>
-
-        <Button className='btn' variant='contained' onClick={()=>handleDownload(inferredImg)}>Download Image</Button>
-      <Button className='btn' id='inferBtn' variant='contained' color='secondary' size='large' onClick={()=>handleInfer(rawImg,setInferredImg)}>Infer</Button>
+      <ImageCard imageUrl={userImg} imageName='uploadedImage' imgText='original'></ImageCard>
+      <div id='infImg'>
+      <ImageCard  imageUrl={inferredImg} imageName='inferredImage' buttonName={"Download"} imgText='inferred'></ImageCard>
       </div>
+
+      <input id='selectImage' type='file' accept='image/*' hidden onChange={(event) => { setUserImg(URL.createObjectURL(event.target.files[0])); setRawImg(event.target.files[0]) }} />
+      <Button className='btn' id='selectImg' variant='contained'><label htmlFor="selectImage">Select Image</label></Button>
+
+      <Button className='btn' variant='contained' onClick={() => handleDownload(inferredImg)}>Download Image</Button>
+      <Button className='btn' id='inferBtn' variant='contained' color='secondary' onClick={() => handleInfer(rawImg, setInferredImg)}>Infer</Button>
 
     </div>
   )
